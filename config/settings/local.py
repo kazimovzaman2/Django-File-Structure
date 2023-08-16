f7e76a2bc8f627a4afaf6cfb3a716222766fff5e
kazimovzaman2 <kazimovzaman2@gmail.com>
2023-08-16 17:52:11 +0400

    go back

--- config/settings/local.py
+++ config/settings/local.py
@@ -1,7 +1,1 @@
 from config.settings.base import *
-
-ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
-
-DOMAIN = "localhost"
-
-EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
--- config/settings/production.py
+++ config/settings/production.py
@@ -1,3 +1,1 @@
 from config.settings.base import *
-
-ADMIN_URL = config("DJANGO_ADMIN_URL")
