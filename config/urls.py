d7e0a869550f42d14d985fe173e3d99ad83d4e90
kazimovzaman2 <kazimovzaman2@gmail.com>
2023-08-16 22:27:22 +0400

    drf

--- config/urls.py
+++ config/urls.py
@@ -2,9 +2,34 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularRedocView,
+    SpectacularSwaggerView,
+)
+from rest_framework_simplejwt.views import TokenBlacklistView
+
 
 urlpatterns = [
     path(settings.ADMIN_URL, admin.site.urls),
+    path(
+        "api/jwt/blacklist/",
+        TokenBlacklistView.as_view(),
+        name="token_blacklist",
+    ),
+    # DRF Spectacular UIs:
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    # DRF Spectacular UIs:
+    path(
+        "api/schema/swagger-ui/",
+        SpectacularSwaggerView.as_view(url_name="schema"),
+        name="swagger-ui",
+    ),
+    path(
+        "api/schema/redoc/",
+        SpectacularRedocView.as_view(url_name="schema"),
+        name="redoc",
+    ),
 ]
 
 if settings.DEBUG:
